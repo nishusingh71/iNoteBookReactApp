422962a4ba5c990a796df60af590a6a4e2737e2e
Prashant Yadav <prashant123.iimt@gmail.com>
2024-08-07 23:32:15 +0530

    Testing

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
-import './App.css';
-import React from 'react';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import About from './components/About';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import NoteState from './context/notes/NoteState';
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes
-} from "react-router-dom";
-import Alert from './components/Alert';
-import { useState} from 'react';
+import "./App.css";
+import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import NoteState from "./context/notes/NoteState";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Alert from "./components/Alert";
+import { useState } from "react";
 
 function App() {
   const [alert, setAlert] = useState(null);
   const showAlert = (message, type) => {
     setAlert({
       msg: message,
-      type: type
+      type: type,
     });
     setTimeout(() => {
       setAlert(null);
     }, 3000);
-  }
+  };
   return (
     <>
       <NoteState>
@@ -33,10 +29,22 @@ function App() {
           <Alert alert={alert} />
           <div className="container">
             <Routes>
-              <Route exact path="/" element={<Home showAlert={showAlert} />}></Route>
+              <Route
+                exact
+                path="/"
+                element={<Home showAlert={showAlert} />}
+              ></Route>
               <Route exact path="/about" element={<About />}></Route>
-              <Route exact path="/login" element={<Login showAlert={showAlert} />}></Route>
-              <Route exact path="/signup" element={<Signup showAlert={showAlert} />}></Route>
+              <Route
+                exact
+                path="/login"
+                element={<Login showAlert={showAlert} />}
+              ></Route>
+              <Route
+                exact
+                path="/signup"
+                element={<Signup showAlert={showAlert} />}
+              ></Route>
             </Routes>
           </div>
         </Router>
