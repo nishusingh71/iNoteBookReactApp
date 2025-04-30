e17d0e50dd4fd5ccd7df4c6835e54d265057f4ee
Nishu Singh <53341807+nishusingh71@users.noreply.github.com>
2025-04-30 13:41:42 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -4,10 +4,7 @@ const mongoose = require("mongoose");
 
 const connectToMongo = async () => {
   try {
-    await mongoose.connect('mongodb+srv://iNoteBook:28opjLspbS67jUqH@inotebook.q6gd2.mongodb.net/?retryWrites=true&w=majority&appName=iNoteBook', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect('mongodb+srv://iNoteBook:OFBQHhxzNIvadwGl@notebooks.mhpjv3y.mongodb.net/iNotes');
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error.message);
